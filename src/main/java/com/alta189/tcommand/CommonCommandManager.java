49cf806bb52d2af85d1473f850166f30923f9570
alta189 <me@alta189.com>
2012-03-19 14:36:35 -0400

    First commit

---
+++ src/main/java/com/alta189/tcommand/CommonCommandManager.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2012 CyborgDev <cyborg@alta189.com>
+ *
+ * This file is part of TerminalCommand
+ *
+ * TerminalCommand is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TerminalCommand is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.alta189.tcommand;
+
+import com.alta189.tcommand.cmd.Command;
+import com.alta189.tcommand.cmd.CommandExecutor;
+import com.alta189.tcommand.cmd.CommandManager;
+import com.alta189.tcommand.cmd.CommandSource;
+
+public class CommonCommandManager extends CommandManager {
+
+	@Override
+	public void execute(CommandSource source, Command command, String[] args) {
+		CommandExecutor executor = command.getExecutor();
+		execute(source, command, args);
+	}
+
+	@Override
+	public void execute(String raw) {
+		//To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public void registerCommand(Command command) {
+		getCommandMap().addCommand(command);
+	}
+
+
+}
