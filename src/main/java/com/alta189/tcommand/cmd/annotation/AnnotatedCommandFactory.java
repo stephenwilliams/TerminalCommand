40a54f4ec8db89f3aa924faa9946b778f8a621ad
alta189 <me@alta189.com>
2012-03-19 21:49:28 -0400

    Formatting - Look away

--- src/main/java/com/alta189/tcommand/cmd/annotation/AnnotatedCommandFactory.java
+++ src/main/java/com/alta189/tcommand/cmd/annotation/AnnotatedCommandFactory.java
@@ -39,7 +39,7 @@ public class AnnotatedCommandFactory {
 			instance = injector.newInstance(clazz);
 		}
 
-		for (Method method : clazz.getMethods()) {;
+		for (Method method : clazz.getMethods()) {
 			if (!Modifier.isStatic(method.getModifiers()) && instance == null) {
 				continue;
 			}
